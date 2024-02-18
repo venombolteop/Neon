9aa30b811f1503a32ca659aed43d20ca42c01285
Abbasxan <138109623+Abbasxan@users.noreply.github.com>
2024-02-18 17:22:25 +0400

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -18,7 +18,7 @@ GBAN_LOG_GROUP_ID = int(os.environ.get("GBAN_LOG_GROUP_ID","-1001674865910"))
 MESSAGE_DUMP_CHAT = int(os.environ.get("MESSAGE_DUMP_CHAT","-1001674865910"))
 WELCOME_DELAY_KICK_SEC = int(os.environ.get("WELCOME_DELAY_KICK_SEC", 600))
 MONGO_URL = os.environ.get("MONGO_URL","mongodb+srv://public:abishnoimf@cluster0.rqk6ihd.mongodb.net/?retryWrites=true&w=majority")
-ARQ_API_KEY = os.environ.get("ARQ_API_KEY","JWHZYN-VYCRDX-ADOCXF-HFABOF-ARQ")
+ARQ_API_KEY = os.environ.get("ARQ_API_KEY","UENTQI-XCFHRE-XFNNBY-YQVXOD-ARQ")
 ARQ_API_URL = os.environ.get("ARQ_API_URL", "https://arq.hamker.dev")
 LOG_MENTIONS = os.environ.get("LOG_MENTIONS", "True").lower() in ["true", "1"]
 RSS_DELAY = int(os.environ.get("RSS_DELAY", 300))
