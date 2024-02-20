c5088a644365f23b22599b3862246ff3031ba2fe
SI_NN_ER_LS <72606082+si-nn-er-ls@users.noreply.github.com>
2024-02-20 09:26:52 +0530

    Update __init__.py

--- wbb/__init__.py
+++ wbb/__init__.py
@@ -33,7 +33,7 @@ from pyrogram.types import Message
 from pyromod import listen
 from Python_ARQ import ARQ
 from telegraph import Telegraph
-from config import *
+#from config import *
 
 #is_config = path.exists("sample_config.py")
 #is_config = path.exists("config.py") yuxaridakin yoxlamaq ucun eledim sabah silecem
@@ -42,12 +42,39 @@ from config import *
 #else:
 #    from sample_config import *
 
-SESSION_STRING="AgFXjN0AtAUKvmL9S4q0FPsekOz0Kro_o5Va_awK8wNPoWiZAfzO23wfgV-NZo4AY7bZ_ojmNigZ-LWOrbbYcUWCTuyvc8QWDZ8mYaC4V3tVhnV3OizTzGooBKcXrnydyRv9_72okkmSOd9MyOV1DTAE32VXXu1wgtHwgW8VwOBVd527mYaWexwBkth4n4fsfMoc-rsWjvPKrXV-ip5fJgYgMZsx5zs6WyR0my_gTjhzSOL3Y3Rk6IyTE7auOUClLU8zSuku5KEzUN-rL5czTerDL9fQ0-ESeskmRbHa-6XEsdCk4YoqQcRRtT2JI86nL0L06DfnJsXQvpHT5Tetdsak0KhOZwAAAAF8uz0RAA"
-USERBOT_PREFIX = USERBOT_PREFIX
-GBAN_LOG_GROUP_ID = GBAN_LOG_GROUP_ID
-WELCOME_DELAY_KICK_SEC = WELCOME_DELAY_KICK_SEC
-LOG_GROUP_ID = LOG_GROUP_ID
-MESSAGE_DUMP_CHAT = MESSAGE_DUMP_CHAT
+
+BOT_TOKEN="6652148246:AAFW45QJL95sK9DywEHzm7DaY4LIpQc76bE" # Get it from @botfather
+
+API_ID=22514909
+
+API_HASH="625b2dfe66926aaf7a41018d07eba8a7"
+
+SESSION_STRING="AgFXjN0AtAUKvmL9S4q0FPsekOz0Kro_o5Va_awK8wNPoWiZAfzO23wfgV-NZo4AY7bZ_ojmNigZ-LWOrbbYcUWCTuyvc8QWDZ8mYaC4V3tVhnV3OizTzGooBKcXrnydyRv9_72okkmSOd9MyOV1DTAE32VXXu1wgtHwgW8VwOBVd527mYaWexwBkth4n4fsfMoc-rsWjvPKrXV-ip5fJgYgMZsx5zs6WyR0my_gTjhzSOL3Y3Rk6IyTE7auOUClLU8zSuku5KEzUN-rL5czTerDL9fQ0-ESeskmRbHa-6XEsdCk4YoqQcRRtT2JI86nL0L06DfnJsXQvpHT5Tetdsak0KhOZwAAAAF8uz0RAA"  # Check Readme to generate sessions
+
+USERBOT_PREFIX="."
+
+SUDO_USERS_ID=[5570119623, 5045429385] # Sudo users have full access to everythin, don't trust anyone
+
+LOG_GROUP_ID=-1001674865910
+
+MONGO_URL="mongodb+srv://public:abishnoimf@cluster0.rqk6ihd.mongodb.net/?retryWrites=true&w=majority"
+
+GBAN_LOG_GROUP_ID=-1001674865910
+
+MESSAGE_DUMP_CHAT=-1001609611579
+
+WELCOME_DELAY_KICK_SEC=300 # Edit if u want
+
+ARQ_API_URL="https://arq.hamker.dev" # Leave it like this
+
+ARQ_API_KEY="UENTQI-XCFHRE-XFNNBY-YQVXOD-ARQ" # Get it from @ARQRobot
+
+LOG_MENTIONS=1 # Make it 1 if you want to enable it
+
+RSS_DELAY=300 # In seconds
+
+PM_PERMIT=1
+
 MOD_LOAD = []
 MOD_NOLOAD = []
 SUDOERS = filters.user()
